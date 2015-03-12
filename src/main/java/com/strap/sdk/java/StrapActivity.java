e5a7242dd0415a0321caf124388a39fcea1a9907
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-03-12 03:30:16 -0400

    classes to expose activity data

---
+++ src/main/java/com/strap/sdk/java/StrapActivity.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.strap.sdk.java;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author marcellebonterre
+ */
+public class StrapActivity {
+    public ArrayList<StrapActivityModel> activities;
+
+    public StrapActivity(ArrayList<StrapActivityModel> activities){
+        this.activities = activities;
+    }
+}
---
+++ src/main/java/com/strap/sdk/java/StrapReport.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.strap.sdk.java;
+
+/**
+ *
+ * @author marcellebonterre
+ */
+public class StrapReport extends StrapActivityModel{
+
+}
