eb90a1414f01f3319f71650e55652c2829e97e93
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-03-12 15:20:08 -0400

    strapReport wraps activity data

--- src/main/java/com/strap/sdk/java/StrapReport.java
+++ src/main/java/com/strap/sdk/java/StrapReport.java
@@ -5,10 +5,22 @@
  */
 package com.strap.sdk.java;
 
+import java.util.Map;
+
 /**
  *
  * @author marcellebonterre
  */
-public class StrapReport extends StrapActivityModel{
+public class StrapReport {
 
+    public String id;
+    public int timestamp;
+    public String date;
+    public String type;
+    public String range;
+    public int count;
+    public String guid;
+    public StrapAggregate aggregate;
+    public Map<String,Object> details;
+    public Object Average;
 }
