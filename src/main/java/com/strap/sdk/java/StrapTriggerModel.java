eec5590bff61087d8784dc100a5ec2680358347a
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-03-21 14:26:25 -0400

    created trigger model

---
+++ src/main/java/com/strap/sdk/java/StrapTriggerModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.strap.sdk.java;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author marcellebonterre
+ */
+public class StrapTriggerModel {
+    String trigger;
+    ArrayList<String> users;
+}
